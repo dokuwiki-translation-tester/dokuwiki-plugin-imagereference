1dc279c5ccdb64f7834facb128b807535f6ff41c
Aagt.p <ap_uekat@gmail.com>
2021-08-05 08:45:13 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aagt.p <ap_uekat@gmail.com>
+ */
+$lang['imgshort']              = 'Rys.';
+$lang['tabshort']              = 'Tab.';
+$lang['imgfull']               = 'Rysunek';
+$lang['tabfull']               = 'Tabela';
